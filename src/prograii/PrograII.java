6410face968e9030f1b1f99090fdb86f6ed06b99
aceb30 <bacabrera@inf.udec.cl>
2022-09-27 10:38:07 -0300

    starting

---
+++ src/prograii/PrograII.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prograii;
+
+/**
+ *
+ * @author bacabrera2021
+ */
+public class PrograII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
