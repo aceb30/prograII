43b757634108c19c7d4f4d0e32dd7105e4daa577
aceb30 <bacabrera@inf.udec.cl>
2022-09-27 11:20:49 -0300

    classes created

--- src/prograii/PrograII.java
+++ src/prograii/PrograII.java
@@ -1,6 +1,47 @@
 
 package prograii;
 
+class Pago{
+    
+}
+class Efectivo extends Pago{
+    
+}
+class Transferencia extends Pago{
+    
+}
+class Tarjeta extends Pago{
+    
+}
+
+class DocTributario {
+    
+}
+class Boleta extends DocTributario{
+    
+}
+class Factura extends DocTributario{
+    
+}
+
+class Cliente{
+    
+}
+class Direccion{
+    
+}
+
+class Articulo{
+    
+}
+
+class DetalleOrden{
+
+}
+
+class OrdenCompra{
+    
+}
 
 public class PrograII {
 
