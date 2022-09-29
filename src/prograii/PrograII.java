6c4762727e78293cb9ba1e39a43aecae39f80ccd
aceb30 <bacabrera2021@inf.udec.cl>
2022-09-29 17:58:03 -0300

    ola

--- src/prograii/PrograII.java
+++ src/prograii/PrograII.java
@@ -1,6 +1,50 @@
 
 package prograii;
 
+<<<<<<< Updated upstream
+=======
+class Pago{
+    
+}
+class Efectivo extends Pago{
+    
+}
+class Transferencia extends Pago{
+    
+}
+class Tarjeta extends Pago{
+    
+}
+
+class DocTributario {
+    
+}
+class Boleta extends DocTributario{
+    
+}
+class Factura extends DocTributario{
+    
+}
+
+class Cliente{
+    
+}
+class Direccion{
+    
+}
+
+class Articulo{
+    
+}
+
+class DetalleOrden{
+
+}
+
+class OrdenCompra{
+    
+}
+>>>>>>> Stashed changes
 
 public class PrograII {
 
