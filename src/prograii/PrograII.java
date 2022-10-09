7a9e3d542fc7dbb09e2cc8696c6a93b3781303b0
aceb30 <bacabrera2021@inf.udec.cl>
2022-10-09 02:28:06 -0300

    IMPLEMENTING STUFF!
    Se hizo toda la parte de cliente, detalle orden y el grueso de ordencompra, pero faltan aún cosas en esa clase ugu y fuera, toi cagao de sueño

--- src/prograii/PrograII.java
+++ src/prograii/PrograII.java
@@ -34,26 +34,61 @@ class Tarjeta extends Pago{
 }
 
 class DocTributario {
-    
     private String numero;
     private String rut;
     private Date fecha;
+    private Direccion direccion;
+    public DocTributario(String num, String r, Date fec, Direccion dir){
+        numero = num;
+        rut = r;
+        fecha = fec;
+        direccion = dir;
+    }
         
 }
 class Boleta extends DocTributario{
+    public Boleta(String num, String r, Date fec){
+        super(num, r, fec, dir);
+    }
+    
     
 }
 class Factura extends DocTributario{
+    public Factura(String num, String r, Date fec){
+        super(num, r, fec, dir);
+    }
     
 }
 
 class Cliente{
     private String nombre;
     private String rut;
+    private Direccion direccion;
+    public Cliente(String nom, String r, String dir){
+        nombre = nom;
+        rut = r;
+        direccion = new Direccion(dir);
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getRut(){
+        return rut;
+    }
+    public String getDireccion(){
+        return direccion.getDireccion();
+    }
+    
     
 }
 class Direccion{
     private String direccion;
+    public Direccion(String dir){
+        direccion = dir;
+    }
+    public String getDireccion(){
+        return direccion;
+    }
 }
 
 class Articulo{
@@ -61,26 +96,66 @@ class Articulo{
     private String nombre;
     private String descripcion;
     private float precio;
+    public Articulo(float pes, String nombr, String descr, float prec){
+       peso = pes;
+       nombre = nombr;
+       descripcion = descr;
+       precio = prec;
+    }
+    public float getPeso(){
+        return peso;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public float getPrecio(){
+        return precio;
+    }
     
 }
 
 class DetalleOrden{
-    
+    private float deuda;
+    private ArrayList<Articulo> lista;
     private int cantidad;
-    public float calcPrecio(){
-        return NULL;
+    public DetalleOrden(){
+        cantidad = 0;
+        lista = new ArrayList<Articulo>();
     }
-    
-    public float calcPrecioSinIVA(){
-        return NULL;
+    public void addArticulo(Articulo art){
+        lista.add(art);
+        cantidad = lista.size();
+    }
+    public float calcPrecio(){
+        float precio = 0;
+        for(int i=0; i<cantidad-1; ++i){
+             precio = precio + lista.get(i).getPrecio();
+        }
+        deuda = precio;
+        return precio;
     }
     
     public float calcIVA(){
-        return NULL;
+        float precio = calcPrecio();
+        float IVA = (float) ((precio * 0.19)/1.19);
+        return IVA;
+    }
+    public float calcPrecioSinIVA(){
+        float precio = calcPrecio();
+        float IVA = calcIVA();
+        float siniva = (float) ((precio - IVA));
+        return siniva;
     }
     
     public float calcPeso(){
-        return NULL;
+        float peso = 0;
+        for(int i=0; i<cantidad-1; ++i){
+             peso = peso + lista.get(i).getPeso();
+        }
+        return peso;
     }
 
 }
@@ -88,10 +163,61 @@ class DetalleOrden{
 class OrdenCompra{
     private Date fecha;
     private String estado;
+    private ArrayList<String> estados;
+    private DetalleOrden detalle;
+    private Cliente cliente;
     public OrdenCompra(){
         fecha = new Date();
-        
+        estados = new ArrayList<String>();
+        estados.add("Creando/editando pedido");
+        estados.add("Pedido creado, obteniendo información del cliente");
+        estados.add("Esperando pago");
+        estados.add("Pago realizado, compra exitosa");
+        estado = estados.get(0);
     }
+    
+    public void add(Articulo art){
+        estado = estados.get(0);
+        detalle.addArticulo(art);
+    }
+    
+    public float calcPrecioSinIVA(){
+        return detalle.calcPrecioSinIVA();
+    }
+    
+    public float calcIVA(){
+        return detalle.calcIVA();
+    }
+    
+    public float calcPrecio(){
+        return detalle.calcPrecio();
+    }
+    
+    public float calcPeso(){
+        return detalle.calcPeso();
+    }
+    
+    public void infCliente(String nombre, String rut, String direccion){
+        estado = estados.get(1);
+        cliente = new Cliente(nombre, rut, direccion);
+    }
+    
+    int numPago = 0;
+    
+    public Boleta crearBoleta(){
+        String num = String.valueOf(numPago);
+        Boleta boleta = new Boleta(num, cliente.getNombre(), fecha,
+        cliente.getDireccion());
+        return boleta;
+    }
+   
+    public Factura crearFactura(){
+        Factura factura = new Factura(String.valueOf(numPago), cliente.getNombre(), fecha
+        ,cliente.getDireccion());
+        return factura;
+    }
+    
+    
 }
 
 
